d2f65165e85018cb12a759641988b79d53a4951f
Armands <armands@tuta.io>
2024-10-21 14:03:00 +0300

    Pievienoju md renderēšanu

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import markdown
 
 # Specify the path to your templates folder
 app = Flask(__name__, template_folder='../frontend/', static_folder='../frontend/static/')
@@ -6,7 +7,14 @@ app = Flask(__name__, template_folder='../frontend/', static_folder='../frontend
 @app.route('/')
 def index():
     """Render home page."""
-    return render_template('index.html')  # Render templates as usual
+
+    # Read the markdown file
+    with open('../notes/main.md', 'r', encoding="utf-8") as file:
+        notes_content = file.read()
+    
+    notes_html = markdown.markdown(notes_content, extensions=['fenced_code'])
+
+    return render_template('index.html', notes=notes_html)  # Render templates as usual
 
 if __name__ == '__main__':
     app.run(debug=True, port=2005)
---
+++ notes/main.md
@@ -0,0 +1,10 @@
+# Ok tātad...
+
+Sākumā vajag izveidot repo
+Githubā to var caur UI vai CMD
+
+Ir komanda:
+```
+gh repo create
+```
+**gh** ir githuba cmd
